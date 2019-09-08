d6b89d53017e8dd5d4d1b52bb1e177252cb5c2c8
Prabha-Karri <53992976+Prabha-Karri@users.noreply.github.com>
2019-09-08 21:10:56 +0100

    Add files via upload

---
+++ WebCodeTask/src/main/java/com/util/Setup.java
@@ -0,0 +1,10 @@
+package com.util;
+
+public interface Setup {
+
+	
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_PATH="src\\test\\resources\\Config\\chromedriver.exe";
+	
+	String AUTOMATION_URL="http://automationpractice.com/index.php";
+}
